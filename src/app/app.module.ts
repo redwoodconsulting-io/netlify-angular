5a8e7b4fc667ceddc6afa16618024980d91e76ae
David Haley <dchaley@gmail.com>
2019-02-22 21:52:42 -0800

    Add angular-bootstrap-md
    * Update Angular to 7.2.6 (from 7.2.0).
    * Install angular-bootstrap-md (+ dependencies).
    * Add ng7 angular-fontawesome integration.
    * Add sample MDB navbar.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library as fontAwesomeLibrary } from '@fortawesome/fontawesome-svg-core';
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,9 +17,17 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MDBBootstrapModule.forRoot(),
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor() {
+    fontAwesomeLibrary.add(faFacebook);
+    fontAwesomeLibrary.add(faInstagram);
+    fontAwesomeLibrary.add(faTwitter);
+  }
+}
